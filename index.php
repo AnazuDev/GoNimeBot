369abb90e28cf816cf8342ae9f38a6839b20c454
AnasBex <AnasBex>
2023-05-12 20:51:15 +0700

    Bot

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'vendor/autoload.php';
+
+use Telegram\Bot\Api;
+
+$telegram = new Api('5578179405:AAFJ5_VQw1glY_318-mybScrNQHGLwr8PME');
+
+$result = $telegram->getWebhookUpdates();
+
+$text = $result["message"]["text"];
+$chat_id = $result["message"]["chat"]["id"];
+
+if ($text == "/start") {
+    $telegram->sendMessage([
+        'chat_id' => $chat_id,
+        'text' => 'Hello, world!'
+    ]);
+}
